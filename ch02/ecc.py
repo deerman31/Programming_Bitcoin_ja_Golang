c1ac2d31ea1546b2f98237cd4908b26bb8faba76
Yoshinari Kusano <yoshinarikusano@Yoshinaris-MacBook-Air.local>
2024-03-03 05:21:10 +0900

    a

--- ch02/ecc.py
+++ ch02/ecc.py
@@ -20,11 +20,21 @@ class Point:
 	def __add__(self, other):
 		if self.a != other.a or self.b != other.b:
 			raise TypeError('Points {}, {} are not on the same curve'.format(self, other))
-
 		if self.x == other.x and self.y != other.y:
 			return self.__class__(None, None, self.a, self.b)
-
 		if self.x is None:
 			return other
 		if other.x is None:
 			return self
+		if self.x != other.x:
+			s = (other.y - self.y) / (other.x - self.x)
+			x3 = s ** 2 - self.x - other.x
+			y3 = s * (self.x - x3) - self.y
+			return self.__class__(x3, y3, self.a, self.b)
+		if self == other and self.y == 0 * self.x:
+			return self.__class__(None, None, self.a, self.b)
+		if self == other:
+			s = ((3 * self.x) ** 2 + self.a) / (2 * self.y)
+			x3 = s ** 2 - 2 * self.x
+			y3 = s * (self.x - x3) - self.y
+			return self.__class__(x3, y3, self.a, self.b)
